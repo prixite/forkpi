5b5372bf4b673e2d02ef176d21459e512e137a8d
bilal-irfan <bilal.irfan@prixite.com>
2023-07-17 12:35:59 +0500

    #1 django command for pins

---
+++ forkpi/records/management/commands/send_sms.py
@@ -0,0 +1,102 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from records.models import Keypair
+
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('log.txt'),
+        logging.StreamHandler()
+    ]
+)
+
+# Alex's Twilio Account SID, Auth Token, and phone number
+twilio_account_sid = os.getenv("TWILIO_ACCOUNT_ID")
+twilio_auth_token = os.getenv("TWILIO_TOKEN")
+twilio_phone_number = os.getenv("TWILIO_NUMBER")
+
+# Alex's OpenAI API Key
+openai_api_key = os.getenv("OPENAI_KEY")
+
+# Initialize Twilio Client
+twilio_client = Client(twilio_account_sid, twilio_auth_token)
+
+# Initialize OpenAI Client
+openai.api_key = openai_api_key
+
+
+def generate_code():
+    code = random.randint(1000, 9999)
+    return str(code) + "#"
+
+
+def generate_one_liner(code):
+    prompt = f"Generate a funny one-liner text message for a secret Florida Panther themed bar. The passcode for this month is {code}."
+    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=60)
+    one_liner = response.choices[0].text.strip()  # remove leading and trailing spaces
+    return one_liner.strip('"')  # remove leading and trailing quotes
+
+
+def generate_image():
+    prompt = "A realistic painting of a light colored florida panther holding an alcoholic beverage playing hockey."
+
+    response = openai.Image.create(
+        prompt=prompt,
+        n=1,
+        size="256x256",
+    )
+
+    # Return the URL of the image
+    return response["data"][0]["url"]
+
+
+def send_sms(phone_number, message, image_url):
+    twilio_client.messages.create(
+        body=message,
+        from_=twilio_phone_number,
+        media_url=[image_url],
+        to=phone_number
+    )
+
+
+class Command(BaseCommand):
+    help = 'Generate new PIN for recipients and save them in pin.txt.'
+
+    def handle(self, *args, **kwargs):
+        try:
+            recipients = Keypair.objects.all()
+            if not recipients:
+                logging.info("No recipients found in the database. Exiting.")
+                return
+
+                # Get existing PINs from the Keypair model
+                existing_pins = [recipient.pin for recipient in recipients]
+
+                # Save existing PINs in pin.txt
+                with open('pin.txt', 'w') as f:
+                    for pin in existing_pins:
+                        f.write(pin + '\n')
+
+            logging.info("Starting code generation.")
+            new_code = generate_code()
+
+            # Write the new code to revolvingcode.txt
+            with open('revolvingcode.txt', 'w') as f:
+                f.write(new_code)
+
+            logging.info("Starting one-liner generation.")
+            message = generate_one_liner(new_code)
+
+            logging.info("Starting image generation.")
+            image_url = generate_image()
+
+            logging.info("Starting SMS sending.")
+            for recipient in recipients:
+                send_sms(recipient.phone_number, message, image_url)
+
+            logging.info("Completed successfully.")
+
+        except Exception as e:
+            logging.error(f"An error occurred: {e}")
