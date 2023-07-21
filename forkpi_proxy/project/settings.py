9839106ad95297cb4b721b49329d02cb8507cf44
Umair Khan <umair.waheed@gmail.com>
2023-07-21 23:05:51 +0500

    Use environ

--- forkpi_proxy/project/settings.py
+++ forkpi_proxy/project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-bdz89+*hvo4#jxdlk(=b$6qu5w-+*1(krk3gukr1_nd0$8=dyz"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
