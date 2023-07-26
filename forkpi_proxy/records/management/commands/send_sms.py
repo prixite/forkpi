c944e586f3a26d2e33193b88b2f53b26fb9b23ef
Umair Khan <umair.waheed@gmail.com>
2023-07-26 14:00:43 +0500

    Run linter

--- forkpi_proxy/records/management/commands/send_sms.py
+++ forkpi_proxy/records/management/commands/send_sms.py
@@ -58,10 +58,7 @@ def generate_image():
 
 def send_sms(phone_number, message, image_url):
     twilio_client.messages.create(
-        body=message,
-        from_=twilio_phone_number,
-        media_url=[image_url],
-        to=phone_number
+        body=message, from_=twilio_phone_number, media_url=[image_url], to=phone_number
     )
 
 
