487e07648df5833c7e51b91b1b26d9e48676d7d8
Umair Khan <umair.waheed@gmail.com>
2023-08-12 15:28:01 +0500

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 docker-compose build
 docker-compose up -d
-docker-compose exec forkpi ./manage.py migrate
-docker-compose exec forkpi ./manage.py collectstatic --noinput;
+docker-compose exec forkpi ./forkpi/manage.py migrate
+docker-compose exec forkpi ./forkpi/manage.py collectstatic --noinput;
 docker-compose ps
